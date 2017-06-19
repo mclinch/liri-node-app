a5436922ccdbf02e2765aac920e0607ca89621b8
mclinch <michael.clinch@gmail.com>
2017-06-18 22:12:00 -0700

    Keys Updated

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'OStDzrAEzmaDl7dFbXf95CR3f',
+  consumer_secret: 'NYLXXiAubLbi2GpFg1TI9bySOSTA5Qu2ToWX58xU5tYwWiEysU',
+  access_token_key: '394390429-uqkZI7OK1hDOAHU3tucxfHSzCGo993ie7yxLAMWT',
+  access_token_secret: '	g6pjo8pk0ZdgVQBZcxOYkMTSQTmIdGZgVeYJUSiDjv36z',
 }
